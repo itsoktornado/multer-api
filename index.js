a309b5a35b39acf05daa915df72dfc03b37d2c34
タツマキ <21521764@gm.uit.edu.vn>
2023-03-06 15:39:10 +0700

    Second commit

--- index.js
+++ index.js
@@ -3,24 +3,38 @@ const express = require('express')
 const app = express()
 const multer = require('multer')
 const path = require('path')
+const fs = require('fs')
+const url = require('url')
+const http = require('http')
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, '\images')
+		cb(null, 'images/')
 	},
 	filename: (req, file, cb) => {
-		cb(null, Date.now() + path.extname(file.originalname))
+		cb(null, file.originalname)
 	}
 })
 
 const upload = multer({ storage: storage })
 
-app.get('/api/upload', (req, res) => {
-	res.render('upload')
+app.get('/api/:filename', (req, res) => {
+	const filename = req.params.filename
+
+	fs.readFile(`./images/${filename}`, (err, data) => {
+		if (err) {
+			console.log('no image found')
+		}
+		else {
+			res.writeHead(200, {'Content-Type': 'image/png'})
+			
+			return res.end(data)
+		}
+	})
 })
 
 app.post('/api/upload', upload.single('image'), (req, res) => {
-	res.send('uploaded')
+	res.send(req.file)
 })
 
 const port = process.env.port || 3000
